07148582df9685963b47e0d41a1965a4f0082dad
Yu Cang <yhcy1993@gmail.com>
2018-12-31 21:16:39 +0800

    interface optimized

---
+++ src/run_task.m
@@ -0,0 +1,19 @@
+clear; close all; clc;
+
+mkdir '../data' 'ChemTbl'
+
+data = xlsread('../data/task.xlsx');
+for k = 1:length(data) 
+    %Clean environment
+    delete('../data/*.txt');
+	delete('../pic/*.png');
+    
+    %Run case
+    mf = data(k, 1);
+    mo = data(k, 2);
+    try
+        SolveFlame1(mf, mo, 0.05, 2501, '../data/ChemTbl');
+    catch
+        fprintf('Case%d failed!\n', k);
+    end
+end
