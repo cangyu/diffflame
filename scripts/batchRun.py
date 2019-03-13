0fb49f3ef3733c4cef109a275ebe5c9cf4965023
Yu Cang <yhcy1993@gmail.com>
2019-03-13 09:39:13 +0800

    add output dir and duplication detect

--- scripts/batchRun.py
+++ scripts/batchRun.py
@@ -13,15 +13,22 @@ else:
         print("Not found!")
         exit(-2)
 
+output_dir = os.path.join('..', 'data')
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
+
 data = np.loadtxt(task_file_path)
 n = len(data)
 
 def counterflow(x):
     mf = x[0]
     mo = x[1]
-    counterflow=subprocess.Popen(["../main.out", "{:f}".format(mf), " {:f}".format(mo)], cwd='./ChemTbl/')
-    counterflow.wait()
+    cur_case_output_name = 'mf={}_mo={}_raw.txt'.format(mf, mo)
+    if not os.path.exists(os.path.join(output_dir, cur_case_output_name)):
+        counterflow=subprocess.Popen(["../src/main.out", "{:f}".format(mf), " {:f}".format(mo)], cwd=output_dir)
+        counterflow.wait()
+    else:
+        print('Case for mf={} mo={} already exists!'.format(mf, mo))
 
-p = Pool(os.cpu_count())
+p = Pool(os.cpu_count()//2)
 p.map(counterflow, data)
-    
