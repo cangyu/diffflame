8b53aff597fcec43e8765b22970101eabb5b624a
Yu Cang <yhcy1993@gmail.com>
2019-03-15 15:06:51 +0800

    add s-curve cmp

---
+++ scripts/cmpTmax.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import math
+import numpy as np
+from matplotlib import pyplot as plt
+
+base_data = np.loadtxt('../ChemTbl/sCurve_grimech.dat', skiprows=1)
+
+kai_st1 = base_data[:, 0]
+Tmax1 = base_data[:, 1]
+
+our_data = np.loadtxt('all_solution.txt')
+kai_st2 = our_data[:, 0]
+Tmax2 = our_data[:, 1]
+
+plt.scatter(np.log10(kai_st1), Tmax1, label='RefSol')
+plt.scatter(np.log10(kai_st2), Tmax2, label='PhySol')
+plt.xlabel('log10(kai_st)')
+plt.ylabel('Tmax/K')
+plt.legend()
+
+plt.show()
